fd7cc71480aa2ec1a8724f6be111353277626bad
Simon Brooke <simon@journeyman.cc>
2019-08-21 16:30:11 +0100

    Outlines of host functions

--- src/beowulf/host.clj
+++ src/beowulf/host.clj
@@ -3,3 +3,36 @@
    be) implemented in Lisp 1.5, which therefore need to be implemented in the
    host language, in this case Clojure.")
 
+;; these are CANDIDATES to be host-implemented. only a subset of them MUST be.
+;; those which can be implemented in Lisp should be, since that aids
+;; portability.
+
+;; RPLACA
+
+;; RPLACD
+
+;; PLUS
+
+;; MINUS
+
+;; DIFFERENCE
+
+;; QUOTIENT
+
+;; REMAINDER
+
+;; ADD1
+
+;; SUB1
+
+;; MAX
+
+;; MIN
+
+;; RECIP
+
+;; FIXP
+
+;; NUMBERP
+
+;;
